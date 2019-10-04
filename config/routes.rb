8ce4484871b2cca405c293b559e24a3ad4fa17c3
ljl19971006 <ljl19971006@gmail.com>
2019-10-04 14:04:24 +0200

    generation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :counters
+  resources :my_models
+  get 'counters/index'
   resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
