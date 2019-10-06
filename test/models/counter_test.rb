d955f8a09d38c6674de6cc3901771bb7868e1a2c
ljl19971006 <ljl19971006@gmail.com>
2019-10-06 19:55:46 +0200

    update

--- test/models/counter_test.rb
+++ test/models/counter_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CounterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should increment counter value" do
+    counter = Counter.new
+    counter.value = 0
+
+    counter.increment!
+
+    assert counter.value == 1
+  end
 end
